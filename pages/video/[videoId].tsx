65d82573517c6f9395795467bf7849b8c83a905d
ChristopherL <anbchris@gmail.com>
2025-09-13 23:57:52 -0400

    beginning to implement ISR for videoId.tsx

--- pages/video/[videoId].tsx
+++ pages/video/[videoId].tsx
@@ -3,21 +3,45 @@ import { useRouter } from "next/router";
 import Modal from "react-modal";
 import clsx from "classnames";
 import styles from "@/styles/Video.module.css";
+import { VideoProps } from "./video.types";
+import { GetStaticPaths, GetStaticProps } from "next";
 
 Modal.setAppElement("#__next");
 
-export default function Video() {
-  const router = useRouter();
-  const { videoId } = router.query;
-
+export const getStaticProps: GetStaticProps = async () => {
+  // fetch data from API or fallback
+  // const res = await fetch();
+  // const videos = res.json();
   const video = {
     title: "Hi cute dog",
     publishTime: "2021-01-01",
-    description:
-      "A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?A big red dog that is super cute, can he get any bigger?",
+    description: "A big red dog that is super cute, can he get any bigger?",
     channelTitle: "Paramount Pictures",
     viewCount: 10000,
   };
+
+  return {
+    props: {
+      video,
+    },
+    revalidate: 10,
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const videosList = ["hJnAHzo4-KI", "BjkIOU5PhyQ", "ETVi5_cnnaE"];
+  const paths = videosList.map((id) => ({
+    params: { id },
+  }));
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export default function Video({ video }: VideoProps) {
+  const router = useRouter();
+  const { videoId } = router.query;
 
   const { title, publishTime, description, channelTitle, viewCount } = video;
 
---
+++ pages/video/video.types.ts
@@ -0,0 +1,9 @@
+export interface VideoProps {
+  video: {
+    title: string;
+    publishTime: string;
+    description: string;
+    channelTitle: string;
+    viewCount: number;
+  };
+}

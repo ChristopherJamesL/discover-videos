3fe884330f03dc07f18037fa81b8c9e9c53507d1
ChristopherL <anbchris@gmail.com>
2025-09-23 18:16:18 -0400

    refactoring hasura and login code

--- lib/db/hasura.ts
+++ lib/db/hasura.ts
@@ -1,23 +1,53 @@
 import { GraphQLResponse, UsersQueryResponse } from "./hasura.types";
 
-export async function fetchGraphQL<T>(
+function getHasuraConfig() {
+  const endpoint = process.env.NEXT_PUBLIC_HASURA_ENDPOINT;
+  const adminSecret = process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET;
+
+  if (!endpoint || !adminSecret) {
+    throw new Error("Missing required HASURA environment variables.");
+  }
+
+  return { endpoint, adminSecret };
+}
+
+export async function isNewUser(token: string, issuer: string) {
+  const operationsDoc = `
+    query isNewUser($issuer: String!) {
+      users(where: {issuer: {_eq: $issuer}}) {
+        email
+        id
+        issuer
+        publicAddress
+      }
+    }
+  `;
+
+  const response = await fetchHasuraGraphQL<UsersQueryResponse>(
+    operationsDoc,
+    "isNewUser",
+    token,
+    { issuer }
+  );
+  const user = response?.data?.users[0];
+  console.log({ user, issuer });
+
+  return response?.data?.users?.length === 0;
+}
+
+export async function fetchHasuraGraphQL<T>(
   operationsDoc: string,
   operationName: string,
+  token: string,
   variables?: Record<string, unknown>
 ): Promise<GraphQLResponse<T>> {
-  const endpoint = process.env.NEXT_PUBLIC_HASURA_ENDPOINT;
-  const secret = process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET;
-  if (!endpoint || !secret)
-    throw new Error(
-      "Missing required hasura environment endpoint or secret variable"
-    );
+  const { endpoint } = getHasuraConfig();
 
   const result = await fetch(endpoint, {
     method: "POST",
     headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkFua3kiLCJpYXQiOjE3NTg1MDEwMDEsImV4cCI6MTc1OTEwNTk0NSwiaHR0cHM6Ly9oYXN1cmEuaW8vand0L2NsYWltcyI6eyJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJ1c2VyIiwieC1oYXN1cmEtYWxsb3dlZC1yb2xlcyI6WyJ1c2VyIiwiYWRtaW4iXSwieC1oYXN1cmEtdXNlci1pZCI6IkFua3kifX0.gYCIq9RJ9rhnTQyXLeKhA9xwQkcp_mKPehrEOAdNDnk",
-      // "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
       // "x-hasura-admin-secret": secret,
     },
     body: JSON.stringify({
@@ -28,17 +58,4 @@ export async function fetchGraphQL<T>(
   });
 
   return await result.json();
-}
-
-export async function fetchMyQuery() {
-  const operationsDoc = `
-    query MyQuery {
-      users {
-        id
-        email
-        issuer
-      }
-    }
-  `;
-  return await fetchGraphQL<UsersQueryResponse>(operationsDoc, "MyQuery", {});
 }
--- pages/api/login/login.ts
+++ pages/api/login/login.ts
@@ -1,19 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { magicAdmin } from "@/lib/magic-server";
 import jwt from "jsonwebtoken";
+import { isNewUser } from "@/lib/db/hasura";
 
 export default async function login(req: NextApiRequest, res: NextApiResponse) {
+  const jwtSecret = process.env.HASURA_GRAPHQL_JWT_SECRET_KEY;
+  if (!jwtSecret)
+    throw new Error("Missing HASURA_GRAPHQL_JWT_SECRET_KEY in env variables");
+
   if (req.method === "POST") {
     try {
       const auth = req.headers.authorization;
       const didToken = auth?.slice(7);
-      console.log("DID TOKEN: ", didToken);
 
-      // invoke magic here
       if (didToken) {
         const metaData = await magicAdmin.users.getMetadataByToken(didToken);
-        // console.log("META DATA: ", metaData);
-        // invoke jwt
+        const { issuer } = metaData;
+        if (!issuer) throw new Error("Missing issuer");
+
         const token = jwt.sign(
           {
             // "sub": "1234567890",
@@ -24,14 +28,16 @@ export default async function login(req: NextApiRequest, res: NextApiResponse) {
             "https://hasura.io/jwt/claims": {
               "x-hasura-default-role": "user",
               "x-hasura-allowed-roles": ["user", "admin"],
-              "x-hasura-user-id": `${metaData.issuer}`,
+              "x-hasura-user-id": `${issuer}`,
             },
           },
-          "the secret goes here"
+          jwtSecret
         );
         console.log("Token: ", token);
+        // Check if user exists
+        const isNewUserQuery = await isNewUser(token, issuer);
 
-        return res.send({ done: true, token });
+        return res.send({ done: true, token, isNewUserQuery });
       }
       return res.status(401).json({ done: false, error: "Missing token" });
     } catch (e) {
